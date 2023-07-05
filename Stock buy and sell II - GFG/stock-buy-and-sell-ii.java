ede434714d099fee5bea1412136a4bd08796d4d7
Vivek kumar <89844805+vivekverm@users.noreply.github.com>
2023-07-05 12:06:42 +0530

    Added solution - LeetHub

---
+++ Stock buy and sell II - GFG/stock-buy-and-sell-ii.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] prices = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.stockBuyAndSell(n, prices);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+
+
+class Solution {
+    public static int stockBuyAndSell(int n, int[] prices) {
+        int[][] dp=new int[n][2];
+        for(int i=0;i<n;i++)Arrays.fill(dp[i],-1);
+        return helper(0,0,n,prices,dp);
+    }
+    static int helper(int i,int buy,int n,int[] prices,int[][] dp){
+        if(i==n){
+            return 0;
+        }
+        if(dp[i][buy]!=-1)return dp[i][buy];
+        int ans=0;
+        if(buy==0){
+            ans=Math.max(-prices[i]+helper(i+1,1,n,prices,dp),helper(i+1,0,n,prices,dp));
+        }
+        else{
+            ans=Math.max(+prices[i]+helper(i+1,0,n,prices,dp),helper(i+1,1,n,prices,dp));
+        }
+        return dp[i][buy]=ans;
+    }
+}
+        
+        
