da05d2257d3689c6dfe54f8b5f455d66771103be
Vivek kumar <89844805+vivekverm@users.noreply.github.com>
2023-07-26 13:48:55 +0530

    Added solution - LeetHub

---
+++ Kth Ancestor in a Tree - GFG/kth-ancestor-in-a-tree.java
@@ -0,0 +1,153 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String arr[] = br.readLine().split(" ");
+	            int k = Integer.parseInt(arr[0]);
+	            int node = Integer.parseInt(arr[1]);
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	    	
+                Solution g = new Solution();
+        		System.out.println(g.kthAncestor(root,k,node));
+                    t--;
+            
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+//User function Template for Java
+
+/*
+Structure of Node class is:
+
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+
+class Solution {
+    public int kthAncestor(Node root, int k, int node) {
+        //Write your code here
+        HashMap<Integer, Node> map = new HashMap<>();
+        Node parent = new Node(-1);
+        helper(root, parent, map);
+        Node temp = null;
+        //Iterating map to find Kth parent of the node
+        while(k-- > 0){
+            if(map.containsKey(node)){
+                temp = map.get(node);
+                node = temp.data;
+            }
+            else{
+                return -1;
+            }
+        }
+        return node;
+    }
+    void helper(Node root, Node parent, HashMap<Integer, Node> map){
+        if(root==null)return;
+        map.put(root.data, parent);
+        helper(root.left, root, map);
+        helper(root.right, root, map);
+    }
+}
